e1932fec5a8138882719ba761a34ce303e5bed7f
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2022-09-03 14:51:34 -0300

    Exercício introdutório do curso de react, que consistiu em adicionar tags HTML para complementar a alteração feita no componente principal da página, o App.js. Foram feitas também modificações no App.css e no index.html com o fim de explorar melhor a estrutura inicial do projeto e como estes arquivos se comunicam para renderizar os componentes da Single Page Application. Foi instalado no VS Code também o plugin ES7 + React/Redux/React-Native, e configurado o emmet para a IDE afim de facilitar a criação de código HTML.

--- app-01-hello-world/src/App.js
+++ app-01-hello-world/src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World, React!</h1>
+      <div style={{width: '90%', border: '1px solid black', padding: '3em', margin: '3em auto'}}>
+        <div style={{textAlign: 'center'}}>
+          <span class="texto-descritivo">
+            <b>Curso de React - Exercício #1:</b> Adicionar qualquer tag HTML no componente principal da aplicação, localizado no App.js.
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
