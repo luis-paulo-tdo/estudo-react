77be64535e49c313caf01f8aa05a2efd029d7492
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2022-09-03 17:16:47 -0300

    Comentários complementares e simplificação da tag de componente.

--- app-02-fundamentos/src/App.js
+++ app-02-fundamentos/src/App.js
@@ -1,9 +1,14 @@
+// Componentes
+import FirstComponent from './components/FirstComponent';
+
+// Styles / CSS
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos de React</h1>
+      <FirstComponent/>
     </div>
   );
 }
--- app-02-fundamentos/src/components/FirstComponent.js
+++ app-02-fundamentos/src/components/FirstComponent.js
@@ -1,7 +1,7 @@
 const FirstComponent = () => {
     return (
         <div>
-            <h1>Meu Primeiro Componente</h1>
+            <h3>Meu Primeiro Componente</h3>
         </div>
     )
 }
