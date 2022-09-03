b8e326bd1cc191e0b5411c2d8272df4b96d8be97
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2022-09-03 17:59:28 -0300

    Implementação de funções diretamente no evento de clique dos botões. Este recurso pode ser adequado apenas para quando houver uma única instrução a ser executada. Caso a complexidade do código seja maior, o mais adequado é atrelá-lo a uma função separada.

--- app-02-fundamentos/src/components/Events.js
+++ app-02-fundamentos/src/components/Events.js
@@ -1,14 +1,22 @@
 const Events = () => {
 
     const handleMyEvent = (event) => {
-        console.log("Ativou o evento!");
+        console.log("Ativou o Evento #1!");
         console.log(event);
     };
 
     return (
         <div>
             <div>
-                <button onClick={handleMyEvent}>Clique Aqui</button>
+                <button onClick={handleMyEvent}>Clique Aqui #1</button>
+            </div>
+            <div>
+                <button onClick={() => console.log("Ativou o Evento #2!")}>Clique Aqui #2</button>
+                <button onClick={() => {
+                    if (true) {
+                        console.log("Este evento está muito complexo para ser colocado inline!")
+                    }
+                }}>Clique Aqui #3</button>
             </div>
         </div>
     )
