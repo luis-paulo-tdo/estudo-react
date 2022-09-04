5e60501788b52e242e75ed6bcffe75ce5d300a53
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2022-09-04 05:34:52 -0300

    Seção #3: Salvando imagens nas pastas public e src/assets para serem incluídas na aplicação.

---
+++ app-03-fundamentos-avancados/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aplicativo #3 - Fundamentos Avançados</h1>
+      {/*Imagem em public*/}
+      <div>
+        <img src="/01.jpg" alt="Universo #1" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
