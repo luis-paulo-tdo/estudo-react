7913026b07aadb0c88dc0b53167d53461cf31517
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2022-09-04 06:18:56 -0300

    Seção #3: Renderização de imagens através da pasta assets por meio de importação. Diferente da importação feita pela pasta public, a importação pela pasta assets é adequada para imagens que podem mudar de acordo com alguma condição ou numa frequência maior.

--- app-03-fundamentos-avancados/src/App.js
+++ app-03-fundamentos-avancados/src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Universe from './assets/01.jpg';
+import ManageData from './components/ManageData';
 
 function App() {
   return (
     <div className="App">
-      <h1>Aplicativo #3 - Fundamentos Avançados</h1>
-      {/*Imagem em public*/}
+      <h1>Aplicação #3 - Fundamentos Avançados</h1>
+      {/* Imagem em public */}
       <div>
-        <img src="/01.jpg" alt="Universo #1" />
+        <img src="/01.jpg" alt="Universo #1" width="50%" />
       </div>
+
+      {/* Imagem em assets */}
+      <div>
+        <img src={Universe} alt="Universo #2" width="50%" />
+      </div>
+      <ManageData/>
     </div>
   );
 }
