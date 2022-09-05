db4c19a00a9a1779297fe4b92e33c0234a9a3356
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2022-09-05 07:37:35 -0300

    Seção #3: Aplicação do recurso previous state para obter o útimo estado de um conjunto de valores e exercer algum processamento sobre ele. Foi criado um botão para apagar os itens de uma determinada lista de forma gradativa, atualizando o state dela e obtendo-o a partir do previous state para ir removendo cada item.

--- app-03-fundamentos-avancados/src/components/ListRender.js
+++ app-03-fundamentos-avancados/src/components/ListRender.js
@@ -3,11 +3,19 @@ import {useState} from 'react'
 const ListRender = () => {
 
     const [list] = useState(['Matheus', 'Pedro', 'Josias', 'Maria']);
-    const [users] = useState([
+    const [users, setUsers] = useState([
         { id: 1, name: 'Matheus', age: 31 },
-        { id: 3687163, name: 'João', age: 28 },
-        { id: 3719837, name: 'Pedro', age: 44 }
+        { id: 2, name: 'João', age: 28 },
+        { id: 3, name: 'Pedro', age: 44 }
     ]);
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevUsers) => {
+            return prevUsers.filter((user) => randomNumber !== user.id);
+        });
+    };
 
     return (
         <div>
@@ -25,6 +33,7 @@ const ListRender = () => {
                     </li>   
                 ))}
             </ul>
+            <button onClick={deleteRandom}>Delete Random User</button>
         </div>
     )
 };
