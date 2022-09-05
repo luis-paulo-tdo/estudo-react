d060dfa0896583cdb20be235f4b017b8f98b13cf
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2022-09-05 07:51:22 -0300

    Seção #3: Ampliando conceito de renderização condicional contendo a situação secundária (else). Na aplicação, o operador ternário foi usado para imprimir o JSX de acordo com a condição do nome atribuído a uma variável.

--- app-03-fundamentos-avancados/src/components/ConditionalRender.js
+++ app-03-fundamentos-avancados/src/components/ConditionalRender.js
@@ -2,13 +2,25 @@ import { useState } from "react"
 
 const ConditionalRender = () => {
 
-    const [x] = useState(true);
+    const [x] = useState(false);
+    const [name, setName] = useState('Matheus');
 
     return (
         <div>
             <h1>Exibição Condicional</h1>
             {x && <p>True</p>}
             {!x && <p>True</p>}
+            <h1>Condição Ternária</h1>
+            {name === 'Matheus' ? (
+                <div>
+                    <p>O nome é João</p>
+                </div>
+            ) : (
+                <div>
+                    <p>O nome não foi encontrado</p>
+                </div>
+            )}
+            <button onClick={() => setName('João')}>Alterar Nome</button>
         </div>
     )
 }
