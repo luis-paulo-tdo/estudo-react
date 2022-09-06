fae0e9c537353302418c8516259372543048e1c9
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2022-09-06 19:20:41 -0300

    Seção #4: Aplicação de CSS inline através do atributo 'style'. Não é a melhor maneira de configurar o CSS, pois reduz a mantenabilidade do código.

--- app-04-css/src/App.js
+++ app-04-css/src/App.js
@@ -10,6 +10,11 @@ function App() {
       {/* CSS de Componente */}
       <MyComponent />
       <p>Paráfrago do App.js</p>
+
+      {/* Inline CSS */}
+      <p style={{ color: 'blue', padding: '5px', border: '1px solid black', width: '30%', margin: '0 auto' }}>
+        Parágrafo estilizado inline
+      </p>
     </div>
   );
 }
