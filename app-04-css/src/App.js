748ddb7e182c67cf499856d48019fa3b70ff153a
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2022-09-06 18:46:09 -0300

    Seção #4: Utilização de CSS para componentes específico, que deve ser escrito num arquivo separado e que contém o mesmo nome do componente onde ele será aplicado. O CSS de componente não é scoped, ou seja, ele pode acabar vazando para outros componentes caso haja regra de colisão.

--- app-04-css/src/App.js
+++ app-04-css/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      {/* CSS Global */}
       <h1>React com CSS</h1>
+
+      {/* CSS de Componente */}
+      <MyComponent />
+      <p>Paráfrago do App.js</p>
     </div>
   );
 }
---
+++ app-04-css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import './MyComponent.css';
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>CSS de Componente</h1>
+        <p>Parágrafo do Componente #2</p>
+        <p className="my-comp-paragraph">Parágrafo do Componente #2</p>
+    </div>
+  )
+}
+
+export default MyComponent
