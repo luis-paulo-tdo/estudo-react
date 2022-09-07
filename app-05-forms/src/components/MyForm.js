dd81d4d00b837eca848e70c9a5f24de2b08c2a6b
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2022-09-07 04:18:23 -0300

    Seção #5: Aplicando uma técnica mais otimizada para a construção de formulários no React, onde a tag label envolve a tag input, tornando opcional o atributo htmlFor. Com isso, a estrutura fica simplificada sem que a semântica seja perdida.

--- app-05-forms/src/components/MyForm.js
+++ app-05-forms/src/components/MyForm.js
@@ -9,6 +9,13 @@ const MyForm = () => {
                 <label htmlFor="name">Nome:</label>
                 <input type="text" name="name" placeholder="Digite o seu nome" />
             </div>
+
+            {/* Label envolvendo o input */}
+            <label>
+                <span>E-mail</span>
+                <input type="email" name="email" placeholder="Digite o seu e-mail" />
+            </label>
+
             <input type="submit" value="Enviar" />
         </form>
     </div>
