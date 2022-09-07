4c457f8aac4d11b0037bdcd1d5cb15b6bc8cd0ae
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2022-09-07 04:40:56 -0300

    Seção #5: Manipulação de valores do formulário utilizando o hook useState, onde a função set é chamada para cada evento de onChange que o input envia, obtendo o valor que vêm dele para atribuí-lo ao state.

--- app-05-forms/src/components/MyForm.js
+++ app-05-forms/src/components/MyForm.js
@@ -1,25 +1,37 @@
 import './MyForm.css';
+import { useState } from 'react';
 
 const MyForm = () => {
-  return (
-    <div>
-        {/* 1 - Criação de Formulário */}
-        <form>
-            <div>
-                <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeholder="Digite o seu nome" />
-            </div>
 
-            {/* Label envolvendo o input */}
-            <label>
-                <span>E-mail</span>
-                <input type="email" name="email" placeholder="Digite o seu e-mail" />
-            </label>
+    // 3 - Gerenciamento de Dados
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
 
-            <input type="submit" value="Enviar" />
-        </form>
-    </div>
-  )
+    const handleName = (e) => {
+        setName(e.target.value);
+    };
+
+    console.log(name);
+
+    return (
+        <div>
+            {/* 1 - Criação de Formulário */}
+            <form>
+                <div>
+                    <label htmlFor="name">Nome:</label>
+                    <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName} />
+                </div>
+
+                {/* 2 - Label envolvendo o input */}
+                <label>
+                    <span>E-mail</span>
+                    <input type="email" name="email" placeholder="Digite o seu e-mail" />
+                </label>
+
+                <input type="submit" value="Enviar" />
+            </form>
+        </div>
+    )
 }
 
 export default MyForm
