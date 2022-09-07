81e8be34a634260c9d7d837b42d712b322a8027f
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2022-09-07 05:13:59 -0300

    Seção #5: Inclusão de um textarea no React utilizando os mesmas conceitos de state e controlled input vistos anteriormente para input text. Foi também feita a inclusão do novo valor no envio dos dados e a limpeza do seu elemento de entrada.

--- app-05-forms/src/components/MyForm.js
+++ app-05-forms/src/components/MyForm.js
@@ -7,6 +7,7 @@ const MyForm = ({ user }) => {
     // 6 - Controlled Inputs
     const [name, setName] = useState(user ? user.name : '');
     const [email, setEmail] = useState(user ? user.email : '');
+    const [bio, setBio] = useState('');
 
     const handleName = (e) => {
         setName(e.target.value);
@@ -17,11 +18,13 @@ const MyForm = ({ user }) => {
         console.log('Enviando o Formulário');
         console.log('Usuário: ', name);
         console.log('E-mail: ', email);
+        console.log('Bio: ', bio);
         console.log('-');
 
         // 7 - Limpar formulário
         setName('');
         setEmail('');
+        setBio('');
     };
 
     return (
@@ -29,16 +32,22 @@ const MyForm = ({ user }) => {
             {/* 1 - Criação de Formulário */}
             {/* 5 - Envio de form */}
             <form onSubmit={handleSubmit}>
-                <div>
+                <div className="form-component">
                     <label htmlFor="name">Nome:</label>
                     <input type="text" name="name" value={name} placeholder="Digite o seu nome" onChange={handleName} />
                 </div>
 
                 {/* 2 - Label envolvendo o input */}
-                <label>
+                <label className="form-component">
                     <span>E-mail</span>
                     {/* 4 - Simplificando manipulação de state */}
                     <input type="email" name="email" value={email} placeholder="Digite o seu e-mail" onChange={(e) => setEmail(e.target.value)} />
+                </label>
+
+                {/* 8 - Textarea */}
+                <label className="form-component">
+                    <span>Biografia</span>
+                    <textarea name="bio" placeholder='Descrição do usuário' rows="5" onChange={(e) => setBio(e.target.value)} value={bio}></textarea>
                 </label>
 
                 <input type="submit" value="Enviar" />
