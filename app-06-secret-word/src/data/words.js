4d4da2292e0c7c763681426e9f823f3cec83f426
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2022-09-07 16:30:06 -0300

    Seção #6: Alteração dos títulos da aplicação para 'Secret Word'; limpeza dos CSS e criação do arquivo que contém todas as palavras disponíveis para advinhação separadas em categoria.

---
+++ app-06-secret-word/src/data/words.js
@@ -0,0 +1,11 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+    classeRagnarok: ["Espadachim, Cruzado", "Cavaleiro", "Cavaleiro Rúnico", "Sicário", "Insurgente", "Algoz", "Renegado", "Mercenário", "Sacerdote", "Shura", "Arqueiro", "Odalisca", "Mercador"],
+    trader: ["Sheik", "Gabriel", "Rafael", "Ogro", "Cohen"],
+    jogo: ["Stardew", "Minecraft", "Diablo", "Ragnarok", "Tibia", "Uno"]
+  };
